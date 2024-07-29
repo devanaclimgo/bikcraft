5a715522382176dc023237c156f10c5c872f2ac8
devanaclimgo <anaclimgo@gmail.com>
2024-07-29 14:58:53 -0300

    orçamento-javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ativar links menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,15 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ativar itens orçamento 
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento =document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
